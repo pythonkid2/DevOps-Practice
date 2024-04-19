ad37d1682c70325af46135de335946aeaf4aa8dc
Mathew Joseph <100591950+pythonkid2@users.noreply.github.com>
2024-04-20 00:08:34 +0530

    Update backend.tf

--- Projects/S3- Remote-backend/backend.tf
+++ Projects/S3- Remote-backend/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "mathewdemo" # change this
+    key            = "mathew/terraform.tfstate"
+    region         = "us-east-2"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
